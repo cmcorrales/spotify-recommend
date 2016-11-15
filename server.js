33acd43530b6925daf52569a2c573c2e81b532d4
Cassandra <cmcorrales3@gmail.com>
2016-11-15 16:06:10 -0500

    create index.html

---
+++ server.js
@@ -0,0 +1,42 @@
+var unirest = require('unirest');
+var express = require('express');
+var events = require('events');
+
+var getFromApi = function(endpoint, args) {
+    var emitter = new events.EventEmitter();
+    var Id = document.getElementByClassName('pure-form'); //selecting value using pure JS -- correct?
+    unirest.get('https://api.spotify.com/v1/' + endpoint)
+           .qs(args)
+           .end(function(response) {
+                if (response.ok) {
+                    emitter.emit('end', response.body);
+                }
+                else {
+                    emitter.emit('error', response.code);
+                }
+            });
+     unirest.get('https://api.spotify.com/v1/artists/' + Id + '/related-artists')
+    return emitter;
+};
+console.log("message");
+var app = express();
+app.use(express.static('public'));
+
+app.get('/search/:name', function(req, res) {
+    var searchReq = getFromApi('search', {
+        q: req.params.name,
+        limit: 1,
+        type: 'artist'
+    });
+
+    searchReq.on('end', function(item) {
+        var artist = item.artists.items[0];
+        res.json(artist);
+    });
+
+    searchReq.on('error', function(code) {
+        res.sendStatus(code);
+    });
+});
+console.log("message2");
+app.listen(process.env.PORT || 8080);
